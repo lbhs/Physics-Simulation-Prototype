f6c6b7413fe311a98dcbe4e016b850ebf5e3babc
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-28 18:06:28 -0700

    Initial commit

---
+++ Assets/MainCanvasScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainCanvasScript : MonoBehaviour
+{
+    public ToggleGroup TG;
+    public List<MonoBehaviour> DrawerScripts = new List<MonoBehaviour>();
+    // Update is called once per frame
+    void Update()
+    {
+        //Gets the current toggle
+        IEnumerator<Toggle> toggleEnum = TG.ActiveToggles().GetEnumerator();
+        toggleEnum.MoveNext();
+        string toggle = toggleEnum.Current.name;
+
+        if (toggle == "Circle")
+        {
+            foreach (MonoBehaviour item in DrawerScripts)
+            {
+                if (item.GetType().Name == "CircleDrawerScript")
+                {
+                    item.enabled = true;
+                }
+                else
+                {
+                    item.enabled = false;
+                }
+            }
+        }
+        else if (toggle == "Square")
+        {
+            foreach (MonoBehaviour item in DrawerScripts)
+            {
+                if (item.GetType().Name == "SquareDrawerScript")
+                {
+                    item.enabled = true;
+                }
+                else
+                {
+                    item.enabled = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SquareDrawerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SquareDrawerScript : MonoBehaviour
+{
+    public GameObject SquarePrefab;
+    public bool MaintainAspectRatio = true;
+    public float scale = 10;
+    public float minMagnitude = 3;
+    public float maxMagnitude = 15;
+    private GameObject square;
+    private Vector3 DownPos;
+    private void Update()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var v3 = Input.mousePosition;
+                v3.z = 10.0f;
+                DownPos = Camera.main.ScreenToWorldPoint(v3);
+
+                square = Instantiate(SquarePrefab, DownPos, Quaternion.identity);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                var v3 = Input.mousePosition;
+                v3.z = 10.0f;
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(v3);
+
+                Vector3 value = (square.transform.position - mousePos) * scale;
+                if (MaintainAspectRatio == true)
+                {
+                    float max = Mathf.Max(value.x, value.y, value.z);
+                    value = new Vector3(max, max, max);
+                }
+                if (value.sqrMagnitude > maxMagnitude * maxMagnitude) //sqrMagnitude is more efficient than magnitude
+                {
+                    value = square.transform.localScale;
+                }
+                if (value.sqrMagnitude < minMagnitude * minMagnitude)
+                {
+                    value = square.transform.localScale;
+                }
+                square.transform.position = (DownPos + mousePos)/2;
+                square.transform.localScale = value;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                //TO-DO apply changes to the save json script
+                square.GetComponent<Rigidbody2D>().simulated = true;
+            }
+        }
+    }
+}
